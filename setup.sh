4f51085a6d2dead01495af2a0d95b717191ec9df
NeuralSurvivor <arvindji0201@gmail.com>
2024-09-13 20:09:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,39 @@
 # setup the entire system for the visual translation
 
 ## translation environment
-git clone https://github.com/AI4Bharat/IndicTrans2.git
-cd IndicTrans2/huggingface_interface
-./install.sh
-cd ../..
+root_dir=$(pwd)
+conda create -n itv2_hf python=3.9 -y
+conda activate itv2_hf
+conda install pip
+python -m pip install --upgrade pip
+python -m pip install torch --extra-index-url https://download.pytorch.org/whl/cu118
+python -m pip install nltk sacremoses pandas regex mock transformers>=4.33.2 mosestokenizer
+python -c "import nltk; nltk.download('punkt')"
+python -m pip install bitsandbytes scipy accelerate datasets flash-attn>=2.1
+python -m pip install sentencepiece
+git clone https://github.com/VarunGumma/IndicTransToolkit
+cd IndicTransToolkit
+python -m pip install --editable ./
+pip install scipy
+cd $root_dir
+conda deactivate
 
 ## scene text erasor environment
-conda env create -f scene_text_eraser.yml
+git clone https://github.com/Onkarsus13/Diff_SceneTextEraser.git
+conda create -n scenee_text_erasor python=3.9 -y
+conda activate scene_text_erasor
+cd Diff_SceneTextEraser
+pip install -e ".[torch]"
+pip install -e .[all,dev,notebooks]
+pip install jax==0.4.23 jaxlib==0.4.23
+cd $root_dir
+conda deactivate
 
-## vtnet environment
-conda env create -f vtnet.yml
+## srnet_plus_2 environment
+conda create -n srnet_plus_2 python=3.8.0
+conda activate srnet_plus_2
+pip install -r srnet_plus_2.txt
+conda deactivate
 
 
 ## imagemagick, pango,cairo,pangocairo
