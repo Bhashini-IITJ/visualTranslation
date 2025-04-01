5dd23b2e4c1e75e7281790a250fd12e1bcfb97a8
10wook <younguk137@naver.com>
2025-04-02 00:48:09 +0900

    temp

--- SRNet/predict.py
+++ SRNet/predict.py
@@ -1,46 +1,29 @@
-# Predict script 
-# author: Niwhskal
-
 import os
 import argparse
-import cfg
 import torch
 from tqdm import tqdm
-import numpy as np
 from model import Generator, Discriminator, Vgg19
 from utils import *
 from datagen import datagen_srnet, example_dataset, To_tensor
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import torchvision.transforms.functional as F
 
+def run_srnet(input_dir, save_dir, checkpoint_path, learning_rate, beta1, beta2, file_mapping, device=None):
+    if device is None:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print_log('Model compiling start.', content_color=PrintColor['yellow'])
 
-def main():
-    
-    parser = argparse.ArgumentParser() 
-    parser.add_argument('--input_dir', help = 'Directory containing xxx_i_s and xxx_i_t with same prefix',
-                        default = cfg.example_data_dir)
-    parser.add_argument('--save_dir', help = 'Directory to save result', default = cfg.predict_result_dir)
-    parser.add_argument('--checkpoint', help = 'ckpt', default = cfg.ckpt_path)
-    args = parser.parse_args()
+    G = Generator(in_channels=3).to(device)
+    D1 = Discriminator(in_channels=6).to(device)
+    D2 = Discriminator(in_channels=6).to(device)
+    vgg_features = Vgg19().to(device)
 
-    assert args.input_dir is not None
-    assert args.save_dir is not None
-    assert args.checkpoint is not None
+    G_solver = torch.optim.Adam(G.parameters(), lr=learning_rate, betas=(beta1, beta2))
+    D1_solver = torch.optim.Adam(D1.parameters(), lr=learning_rate, betas=(beta1, beta2))
+    D2_solver = torch.optim.Adam(D2.parameters(), lr=learning_rate, betas=(beta1, beta2))
 
-    print_log('model compiling start.', content_color = PrintColor['yellow'])
-
-    G = Generator(in_channels = 3).to(device)
-    D1 = Discriminator(in_channels = 6).to(device)
-    D2 = Discriminator(in_channels = 6).to(device)  
-    vgg_features = Vgg19().to(device)   
-      
-    G_solver = torch.optim.Adam(G.parameters(), lr=cfg.learning_rate, betas = (cfg.beta1, cfg.beta2))
-    D1_solver = torch.optim.Adam(D1.parameters(), lr=cfg.learning_rate, betas = (cfg.beta1, cfg.beta2))
-    D2_solver = torch.optim.Adam(D2.parameters(), lr=cfg.learning_rate, betas = (cfg.beta1, cfg.beta2))
-
-    checkpoint = torch.load(args.checkpoint)
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
     G.load_state_dict(checkpoint['generator'])
     D1.load_state_dict(checkpoint['discriminator1'])
     D2.load_state_dict(checkpoint['discriminator2'])
@@ -49,64 +32,56 @@ def main():
     D2_solver.load_state_dict(checkpoint['d2_optimizer'])
 
     trfms = To_tensor()
-    example_data = example_dataset(data_dir= args.input_dir, transform = trfms)
-    example_loader = DataLoader(dataset = example_data, batch_size = 1, shuffle = False)
+    example_data = example_dataset(data_dir=input_dir, transform=trfms)
+    example_loader = DataLoader(dataset=example_data, batch_size=1, shuffle=False)
     example_iter = iter(example_loader)
 
-    print_log('Model compiled.', content_color = PrintColor['yellow'])
-
-    print_log('Predicting', content_color = PrintColor['yellow'])
+    print_log('Model compiled.', content_color=PrintColor['yellow'])
+    print_log('Predicting', content_color=PrintColor['yellow'])
 
     G.eval()
     D1.eval()
     D2.eval()
 
     with torch.no_grad():
+        for step in tqdm(range(len(example_data))):
+            try:
+                inp = next(example_iter)
+            except StopIteration:
+                example_iter = iter(example_loader)
+                inp = next(example_iter)
 
-      for step in tqdm(range(len(example_data))):
+            i_t = inp[0].to(device)
+            i_s = inp[1].to(device)
+            name = str(inp[2][0])
 
-        try:
+            # Extract numeric part of name and lookup original name
+            try:
+                numeric_name = int(name.split('_')[0])
+            except ValueError:
+                numeric_name = None
 
-          #   inp = example_iter.next()
-          inp = next(example_iter)
-                
+            original_name = file_mapping.get(numeric_name, name)
 
-        except StopIteration:
+            o_sk, o_t, o_b, o_f = G(i_t, i_s, (i_t.shape[2], i_t.shape[3]))
 
-          example_iter = iter(example_loader)
-         #   inp = example_iter.next()
-          inp = next(example_iter)
-                
+            o_sk = o_sk.squeeze(0).detach().to('cpu')
+            o_t = o_t.squeeze(0).detach().to('cpu')
+            o_b = o_b.squeeze(0).detach().to('cpu')
+            o_f = o_f.squeeze(0).detach().to('cpu')
 
-        i_t = inp[0].to(device)
-        i_s = inp[1].to(device)
-        name = str(inp[2][0])
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
 
-        o_sk, o_t, o_b, o_f = G(i_t, i_s, (i_t.shape[2], i_t.shape[3]))
+            o_sk = F.to_pil_image(o_sk)
+            o_t = F.to_pil_image((o_t + 1) / 2)
+            o_b = F.to_pil_image((o_b + 1) / 2)
+            o_f = F.to_pil_image((o_f + 1) / 2)
 
-        o_sk = o_sk.squeeze(0).detach().to('cpu')
-        o_t = o_t.squeeze(0).detach().to('cpu')
-        o_b = o_b.squeeze(0).detach().to('cpu')
-        o_f = o_f.squeeze(0).detach().to('cpu')
+            input_size = (i_s.shape[2], i_s.shape[3])
+            o_f = F.resize(o_f, input_size)
 
-        if not os.path.exists(args.save_dir):
-            os.makedirs(args.save_dir)
+            # Save output using the original name
+            o_f.save(os.path.join(save_dir, f"{original_name}.png"))
 
-        o_sk = F.to_pil_image(o_sk)
-        o_t = F.to_pil_image((o_t + 1)/2)
-        o_b = F.to_pil_image((o_b + 1)/2)
-        o_f = F.to_pil_image((o_f + 1)/2)
-                        
-        o_f.save(os.path.join(args.save_dir, name + 'o_f.png'))
-
-        #Uncomment the following if you need to save the rest of the predictions
-
-        #o_sk.save(os.path.join(args.save_dir, name + 'o_sk.png'))
-        #o_t.save(os.path.join(savedir, name + 'o_t.png'))
-        #o_b.save(os.path.join(savedir, name + 'o_b.png'))
-
-            
-if __name__ == '__main__':
-    main()
-    print_log('predicting finished.', content_color = PrintColor['yellow'])
-
+    print_log('Predicting finished.', content_color=PrintColor['yellow'])
