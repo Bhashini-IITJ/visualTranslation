51d80cd806eed48244ab830ead84adaedbd1eb46
한영욱 <70461240+10wook@users.noreply.github.com>
2025-04-01 22:16:29 +0900

    Hyw/srnet/upload (#30)
    * hyw/srnet - ing
    
    * srnet upload

---
+++ SRNet/cfg.py
@@ -0,0 +1,43 @@
+gpu = 0
+
+lt = 1.
+lt_alpha = 1.
+lb = 1.
+lb_beta = 10.
+lf = 1.
+lf_theta_1 = 10.
+lf_theta_2 = 1.
+lf_theta_3 = 500.
+epsilon = 1e-8
+
+# train
+learning_rate = 1e-4 
+decay_rate = 0.9
+beta1 = 0.9
+beta2 = 0.999 
+max_iter = 500000
+show_loss_interval = 50
+write_log_interval = 50
+save_ckpt_interval = 1000
+gen_example_interval = 1000
+checkpoint_savedir = 'logs/'
+ckpt_path = './content/kor_kor_eng.model'
+
+# data
+batch_size = 8
+data_shape = [64, None]
+data_dir = './datasets/srnet_data'
+i_t_dir = 'i_t'
+i_s_dir = 'i_s'
+t_sk_dir = 't_sk'
+t_t_dir = 't_t'
+t_b_dir = 't_b'
+t_f_dir = 't_f'
+mask_t_dir = 'mask_t'
+example_data_dir = 'labels'
+example_result_dir = 'custom_feed/gen_logs'
+
+# predict
+predict_ckpt_path = None
+predict_data_dir = None
+predict_result_dir = 'custom_feed/result'
---
+++ SRNet/datagen.py
@@ -0,0 +1,106 @@
+# author: Niwhskal
+# github : https://github.com/Niwhskal/SRNet
+import re
+import os
+from skimage import io
+from skimage.transform import resize
+import numpy as np
+import random
+import cfg
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+
+class datagen_srnet(Dataset):
+    def __init__(self, cfg, torp = 'train', transforms = None):
+        
+        if(torp == 'train'):
+            self.data_dir = cfg.data_dir
+            self.t_b_dir = cfg.t_b_dir
+            self.batch_size = cfg.batch_size
+            self.data_shape = cfg.data_shape
+            self.name_list = os.listdir(os.path.join(self.data_dir, self.t_b_dir))
+      
+    def __len__(self):
+        return len(self.name_list)
+    
+    def __getitem__(self, idx):
+        
+        img_name = self.name_list[idx]
+        # print(self.name_list)
+        # number = re.match(r'^\d+', img_name).group()
+        # print(number)
+        
+        i_t = io.imread(os.path.join(cfg.data_dir, cfg.i_t_dir, img_name)) #원본
+        # i_t = io.imread(os.path.join(cfg.data_dir, cfg.t_b_dir, img_name))
+        i_s = io.imread(os.path.join(cfg.data_dir, cfg.i_s_dir, img_name))
+        t_sk = io.imread(os.path.join(cfg.data_dir, cfg.t_sk_dir, img_name), as_gray = True)
+        t_t = io.imread(os.path.join(cfg.data_dir, cfg.t_t_dir, img_name))
+        t_b = io.imread(os.path.join(cfg.data_dir, cfg.t_b_dir, img_name))
+        t_f = io.imread(os.path.join(cfg.data_dir, cfg.t_f_dir, img_name))
+        mask_t = io.imread(os.path.join(cfg.data_dir, cfg.mask_t_dir, img_name), as_gray = True)
+        
+        # t_b = io.imread(os.path.join(cfg.data_dir, cfg.t_b_dir, number+ '_'+cfg.t_b_dir +'.png'))
+        # # print(t_b)
+        # i_t = io.imread(os.path.join(cfg.data_dir, cfg.i_t_dir, number+ '_'+cfg.i_t_dir +'.png'))
+        # i_s = io.imread(os.path.join(cfg.data_dir, cfg.i_s_dir,  number+ '_'+cfg.i_s_dir +'.png'))
+        # t_sk = io.imread(os.path.join(cfg.data_dir, cfg.t_sk_dir, number+ '_'+cfg.t_sk_dir +'.png'), as_gray = True)
+        # t_t = io.imread(os.path.join(cfg.data_dir, cfg.t_t_dir, number+ '_'+cfg.t_t_dir +'.png'))
+        # t_f = io.imread(os.path.join(cfg.data_dir, cfg.t_f_dir, number+ '_'+cfg.t_f_dir +'.png'))
+        # mask_t = io.imread(os.path.join(cfg.data_dir, cfg.mask_t_dir, number+ '_'+cfg.mask_t_dir +'.png'), as_gray = True)
+        
+        
+
+        return [i_t, i_s, t_sk, t_t, t_b, t_f, mask_t]
+        
+
+class example_dataset(Dataset):
+    
+    def __init__(self, data_dir = cfg.example_data_dir, transform = None):
+        
+        self.files = os.listdir(data_dir)
+        self.files = [i.split('_')[0] + '_' for i in self.files]
+        self.files = list(set(self.files))
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.files)
+    
+    def __getitem__(self, idx):
+        
+        img_name = self.files[idx]
+        
+        i_t = io.imread(os.path.join(cfg.example_data_dir, img_name + 'i_t.png'))
+        i_s = io.imread(os.path.join(cfg.example_data_dir, img_name + 'i_s.png'))
+        h, w = i_t.shape[:2]
+        scale_ratio = cfg.data_shape[0] / h
+        to_h = cfg.data_shape[0]
+        to_w = int(round(int(w * scale_ratio) / 8)) * 8
+        to_scale = (to_h, to_w)
+        
+        i_t = resize(i_t, to_scale, preserve_range=True)
+        i_s = resize(i_s, to_scale, preserve_range=True)
+        
+        sample = (i_t, i_s, img_name)
+        
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample
+    
+        
+class To_tensor(object):
+    def __call__(self, sample):
+        
+        i_t, i_s, img_name = sample
+
+        i_t = i_t.transpose((2, 0, 1)) /127.5 -1
+        i_s = i_s.transpose((2, 0, 1)) /127.5 -1
+
+        i_t = torch.from_numpy(i_t)
+        i_s = torch.from_numpy(i_s)
+
+        return (i_t.float(), i_s.float(), img_name)
+        
+    
